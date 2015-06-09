88b4a5a9bbccd7b1b40f0a5b87d0ebc3f1d465ec
Anita <asovay@gmail.com>
2015-06-08 17:57:15 -0700

    Level 3

---
+++ beginner/level_3.rb
@@ -0,0 +1,12 @@
+class Player
+  
+  def play_turn(warrior)
+    if (warrior.feel.empty?) && (warrior.health < 20)
+     warrior.rest! 
+    elsif warrior.feel.empty?
+      warrior.walk!
+    else
+      warrior.attack!
+    end
+  end
+end
